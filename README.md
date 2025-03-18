524b09d31a2d357eaf60c0ada2b1487501d862b4
Wutiany <1392508773@qq.com>
2025-03-18 23:57:42 +0800

    project init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cpp_project_study
 学习cmake构建项目，googletest测试框架，以及算法编程等
+
+## 学习使用cmake构建项目
+* 基础cmake框架
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main()
+{
+    std::cout << "this is main cpp file" << std::endl;
+    return 0;
+}
