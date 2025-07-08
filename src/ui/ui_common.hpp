924776f6b68516ee2cfe64b809a7a8bf5673f535
Wuty <1392508773@qq.com>
2025-07-08 23:22:17 +0800

    add: ui common type file

---
+++ src/ui/ui_common.hpp
@@ -0,0 +1,20 @@
+#ifndef __UI_UI_COMMON_HPP__
+#define __UI_UI_COMMON_HPP__
+
+#include <stdio.h>
+
+namespace ui {
+enum class ProtocolType 
+{
+    UDP = 0,
+    TCP_CLIENT = 1,
+    TCP_SERVER = 2,
+    INVALIED = 0xFF,
+}ProtocolType;
+
+
+}
+
+
+
+#endif
